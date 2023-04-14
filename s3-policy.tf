8e27a5db7144b8a2b75cfbe0ecc61a9114191a84
AlisherMuhtarov <therealkimbladi@gmail.com>
2023-04-14 05:57:00 -0500

    s3-testing

---
+++ s3-policy.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "s3-bucket-policy-testing"
+}
+
+resource "aws_s3_bucket_acl" "test_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::*:user/*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::my-bucket-name/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
